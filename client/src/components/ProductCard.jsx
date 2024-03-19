43c5d537f003415c6b1ea5ec609fb4bdd61684fb
Maurice Mwanza <mauricedevs@gmail.com>
2024-03-19 12:23:56 +0300

    Product Screen

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,75 +1,86 @@
-
-import { Box, Image, Text,Badge, Flex, IconButton, Skeleton  } from '@chakra-ui/react';
-import {BiExpand} from 'react-icons/bi'
-import React from 'react'
-import {addTOFavorites, removeFromFavorites } from '../redux/actions/productActions';
-import { useSelector, useDispatch } from 'react-redux'
+import { Box, Image, Text, Badge, Flex, IconButton, Skeleton } from "@chakra-ui/react";
+import { BiExpand } from "react-icons/bi";
+import React, { useState } from "react";
+import { addTOFavorites, removeFromFavorites } from "../redux/actions/productActions";
+import { useSelector, useDispatch } from "react-redux";
 import { MdOutlineFavorite, MdOutlineFavoriteBorder } from "react-icons/md";
+import { Link as ReactLink } from "react-router-dom";
 
+const ProductCard = ({ product, loading }) => {
+	const dispatch = useDispatch();
+	const { favorites } = useSelector((state) => state.product);
+	const [isShown, setIsShown] = useState(false);
 
-const ProductCard = ({product, loading}) => {
-    const dispatch = useDispatch();
-    const {favorites } = useSelector((state) => state.product);
+	return (
+		<Skeleton isLoaded={!loading}>
+			<Box
+				_hover={{ transform: "scale(1.1)", transitionDuration: "0.5s" }}
+				borderWidth='1px'
+				overflow='hidden'
+				p='4'
+				shadow='md'
+			>
+				<Image
+					onMouseEnter={() => setIsShown(true)}
+					onMouseLeave={() => setIsShown(false)}
+					src={product.images[isShown && product.images.length === 2 ? 1 : 0]}
+					fallbackSrc='/no-image.jpg'
+					alt={product.name}
+					height='150px'
+				/>
+				{product.stock < 5 ? (
+					<Badge colorScheme='yellow'>only {product.stock} left</Badge>
+				) : product.stock < 1 ? (
+					<Badge colorScheme='red'>Sold out</Badge>
+				) : (
+					<Badge colorScheme='green'>In Stock</Badge>
+				)}
+				{product.productIsNew && (
+					<Badge ml='2' colorScheme='purple'>
+						New
+					</Badge>
+				)}
+				<Text noOfLines={1} fontSize='xl' fontWeight='semibold' mt='2'>
+					{product.brand} {` `} {product.name}
+				</Text>
+				<Text noOfLines={1} fontSize='md' color='gray.600'>
+					{product.subtitle} {` `} {product.name}
+				</Text>
 
+				<Flex justify='space-between' alignItems='center' mt='2'>
+					<Badge colorScheme='cyan'>{product.category}</Badge>
+					<Text fontSize='xl' fontWeight='semibold' color='cyan.600'>
+						Ksh.{product.price}
+					</Text>
+				</Flex>
+				<Flex justify='space-between' mt='2'>
+					{favorites.includes(product._id) ? (
+						<IconButton
+							icon={<MdOutlineFavorite size='20px' />}
+							colorScheme='cyan'
+							size='sm'
+							onClick={() => dispatch(removeFromFavorites(product._id))}
+						/>
+					) : (
+						<IconButton
+							icon={<MdOutlineFavoriteBorder size='20px' />}
+							colorScheme='cyan'
+							size='sm'
+							onClick={() => dispatch(addTOFavorites(product._id))}
+						/>
+					)}
 
-  return (
-        <Skeleton isLoaded={!loading} _hover={{size: 1.5}} >
-            <Box
-            _hover={{transform:'scale(1.1)', transitionDuration:'0.5s'}}
-            borderWidth='1px'
-            overflow='hidden'
-            p='4'
-            shadow='md'>
-                <Image 
-                src={product.images[0]} 
-                fallbackSrc='/no-image.jpg'  
-                alt={product.name} 
-                height='150px' 
-                />
-                {product.stock < 5 ? (
-                    <Badge colorScheme='yellow'>only { product.stock } left</Badge>
-                ): product.stock < 1 ?(
-                    <Badge colorScheme='red'>Sold out</Badge>
-                ): (<Badge colorScheme='green'>In Stock</Badge>)}
-                {product.productIsNew && (
-                    <Badge ml='2' colorScheme='purple'>
-                        New
-                    </Badge>
-                )}
-                <Text noOfLines={1} fontSize='xl' fontWeight='semibold' mt='2'>
-                    {product.brand} {` `} {product.name}
-                </Text>
-                <Text noOfLines={1} fontSize='md' color='gray.600'>
-                    {product.subtitle} {` `} {product.name}
-                </Text>
+					<IconButton
+						icon={<BiExpand size='20' />}
+						as={ReactLink}
+						to={`/product/${product._id}`}
+						colorScheme='cyan'
+						size='sm'
+					/>
+				</Flex>
+			</Box>
+		</Skeleton>
+	);
+};
 
-                <Flex justify='space-between' alignItems='center' mt='2'>
-                    <Badge colorScheme='cyan'>{product.category}</Badge>
-                    <Text fontSize='xl' fontWeight='semibold' color='cyan.600'>
-                    Ksh.{product.price}
-                </Text>
-                </Flex>
-                <Flex justify='space-between' mt='2'>
-                    { favorites.includes(product._id)? (
-                        <IconButton icon={<MdOutlineFavorite size='20px' />} 
-                         colorScheme='cyan'
-                         size='sm'
-                         onClick={() => dispatch(removeFromFavorites(product._id))} 
-                         />
-                    ) : (
-                        <IconButton icon={<MdOutlineFavoriteBorder size='20px' />} 
-                         colorScheme='cyan'
-                         size='sm'
-                         onClick={() => dispatch(addTOFavorites(product._id))} 
-                         />
-                    ) }
-
-                <IconButton icon={<BiExpand size='20'/>} colorScheme='cyan' size='sm'/>
-                </Flex>
-                    
-            </Box>
-        </Skeleton>
-  );
-}
-
-export default ProductCard
+export default ProductCard;
--- server/model/Product.js
+++ server/model/Product.js
@@ -1,54 +1,64 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-    },
-    images:{
-        type: Array,
-        required: true,
-        default: [],
-    },
-    brand:{
-        type: String,
-        required: true,
-    },
-    category:{
-        type: String,
-        required: true,
-    },
-    reviews:{
-        type: Array,
-        required: true,
-        default: [],
-    },
-    rating:{
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    numberOfReviews:{
-        type: Number,
-        required: true,
-    },
-    price:{
-        type: Number,
-        required: true,
-    },
-    stock:{
-        type: Number,
-        required: true,
-    },
-    productIsNew:{
-        type: Boolean,
-        required: true,
-    },
-    stripeId:{
-        type: Boolean,
-    },
-} , {timestamps: true});
+const reviewSchema = new mongoose.Schema(
+	{
+		name: { type: string, required: true },
+		rating: { type: Number, required: true },
+		comment: { type: string, required: true },
+		title: { type: string, required: true },
+		user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+	},
+	{ timestamps: true }
+);
 
-const Product = mongoose.model('Product', productSchema);
+const productSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		images: {
+			type: Array,
+			required: true,
+			default: [],
+		},
+		brand: {
+			type: String,
+			required: true,
+		},
+		category: {
+			type: String,
+			required: true,
+		},
+		reviews: [reviewSchema],
+		rating: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		numberOfReviews: {
+			type: Number,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		stock: {
+			type: Number,
+			required: true,
+		},
+		productIsNew: {
+			type: Boolean,
+			required: true,
+		},
+		stripeId: {
+			type: Boolean,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
